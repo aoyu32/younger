24545491c18f61c5bafe22d48d2a5b3bc8198501
“yuyongtao” <3256515381@qq.com>
2026-04-26 23:53:58 +0800

    feat: 视频播放页面右侧内容

--- src/view/video/index.vue
+++ src/view/video/index.vue
@@ -7,11 +7,19 @@
         </div>
         <div class="meta">
           <div class="play-count flex-x">
-            <img src="@/assets/video/icon_play_count.svg" alt="" class="icon-size-16 mr-4" />
+            <img
+              src="@/assets/video/icon_play_count.svg"
+              alt=""
+              class="icon-size-16 mr-4"
+            />
             <span>2000万</span>
           </div>
           <div class="date flex-x">
-            <img src="@/assets/video/icon_publish_time.svg" alt="" class="icon-size-16 mr-4" />
+            <img
+              src="@/assets/video/icon_publish_time.svg"
+              alt=""
+              class="icon-size-16 mr-4"
+            />
             <span>2026-4-10 20:21:00</span>
           </div>
         </div>
@@ -19,24 +27,38 @@
       <div class="player-wrapper">
         <video-player class="video-player">
           <video-skin class="video-skin">
-            <hls-video :src="videoSource" playsinline :poster="videoSource"></hls-video>
+            <hls-video
+              :src="videoSource"
+              playsinline
+              :poster="videoSource"
+            ></hls-video>
           </video-skin>
         </video-player>
       </div>
     </div>
-    <div class="video-right"></div>
+    <div class="video-right">
+      <div class="author">
+        <div class="avatar">
+          <img src="@/assets/avatar.png" alt="" />
+        </div>
+        <div class="info">
+          <div class="name"><h3>小满即安</h3></div>
+          <div class="desc"><p>我是一个前端开发</p></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import '@videojs/html/video/player'
-import '@videojs/html/video/skin'
-import '@videojs/html/media/hls-video'
-import '@videojs/html/background/player'
-import '@videojs/html/background/skin'
-import '@videojs/html/background/video'
-const videoSource = ref('https://vjs.zencdn.net/v/oceans.mp4')
+import { ref } from 'vue';
+import '@videojs/html/video/player';
+import '@videojs/html/video/skin';
+import '@videojs/html/media/hls-video';
+import '@videojs/html/background/player';
+import '@videojs/html/background/skin';
+import '@videojs/html/background/video';
+const videoSource = ref('https://vjs.zencdn.net/v/oceans.mp4');
 </script>
 <style lang="scss" scoped>
 @use './index.scss' as *;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3001,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
