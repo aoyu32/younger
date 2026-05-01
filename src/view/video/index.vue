1a40cb4d35b142ecfd45e3a010e2a556eb3dc72f
“yuyongtao” <3256515381@qq.com>
2026-05-01 14:55:13 +0800

    fix: 视频信息头部布局调整

--- src/view/video/index.vue
+++ src/view/video/index.vue
@@ -1,84 +1,80 @@
 <template>
   <div class="video-container">
     <div class="video-wrapper">
-      <div class="content">
-        <div class="video-info">
-          <div class="title">
-            <h3>
-              前端开发最强简历攻略，专治找不到工作，来我这就对了，保你年薪百万,我是一个前端开发,我是一个前端开发,我是一个前端开发
-            </h3>
-          </div>
-          <div class="meta">
-            <div class="play-count flex-x">
-              <img
-                src="@/assets/video/icon_play_count.svg"
-                alt=""
-                class="icon-size-16 mr-4"
-              />
-              <span>2000万</span>
-            </div>
-            <div class="date flex-x">
-              <img
-                src="@/assets/video/icon_publish_time.svg"
-                alt=""
-                class="icon-size-16 mr-4"
-              />
-              <span>2026-4-10 20:21:00</span>
-            </div>
-          </div>
+      <div class="video-info flex-column">
+        <div class="title">
+          <h3>
+            <!-- 前端开发最强简历攻略，专治找不到工作，来我这就对了，保你年薪百万,我是一个前端开发,我是一个前端开发,我是一个前端开发 -->
+            {{ data.title }}
+          </h3>
         </div>
-        <div class="author-info">
-          <div class="author-wrapper">
-            <div class="avatar">
-              <img src="@/assets/avatar.png" alt="" />
-            </div>
-            <div class="info">
-              <div class="name"><h3 class="ellipsis">小满即安</h3></div>
-              <div class="desc">
-                <p class="ellipsis">
-                  我是一个前端开发                  我是一个前端开发
-                </p>
-              </div>
-            </div>
+        <div class="meta">
+          <div class="meta-item flex-x">
+            <AoImage
+              src="@/assets/video/icon_play_count.svg"
+              width="16px"
+              height="16px"
+            />
+            <span>{{ data.meta.playCount }}万</span>
+          </div>
+          <div class="meta-item flex-x">
+            <AoImage
+              src="@/assets/video/icon_publish_time.svg"
+              width="16px"
+              height="16px"
+            />
+            <span>{{ data.meta.publishTime }}</span>
+          </div>
+          <div class="meta-item flex-x">
+            <AoImage
+              src="@/assets/video/icon_author.svg"
+              width="16px"
+              height="16px"
+            />
+            <span>作者：{{ data.meta.author }}</span>
+          </div>
+          <div class="meta-item flex-x" v-if="data.meta.authRequire">
+            <AoImage
+              src="@/assets/video/icon_prohibit.svg"
+              width="16px"
+              height="16px"
+            />
+            <span>未经作者授权，禁止转载</span>
           </div>
         </div>
       </div>
 
+      <!-- 视频区域 -->
       <div class="player-wrapper">
-        <video-player class="video-player">
-          <video-skin class="video-skin">
-            <hls-video
-              :src="videoSource"
-              playsinline
-              :poster="videoSource"
-            ></hls-video>
-          </video-skin>
-        </video-player>
+        <VideoPlayer :src="data.video.src" :poster="data.video.poster" />
+      </div>
+
+      <div class="video-comment">
+        <div class="comment"></div>
+        <div class="recommand"></div>
       </div>
     </div>
-    <!-- <div class="video-right">
-      <div class="author">
-        <div class="avatar">
-          <img src="@/assets/avatar.png" alt="" />
-        </div>
-        <div class="info">
-          <div class="name"><h3>小满即安</h3></div>
-          <div class="desc"><p>我是一个前端开发</p></div>
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import '@videojs/html/video/player';
-import '@videojs/html/video/skin';
-import '@videojs/html/media/hls-video';
-import '@videojs/html/background/player';
-import '@videojs/html/background/skin';
-import '@videojs/html/background/video';
-const videoSource = ref('https://vjs.zencdn.net/v/oceans.mp4');
+import VideoPlayer from '@/components/video-player/index.vue';
+import AoImage from '@/components/ao-image/index.vue';
+
+const data = ref({
+  title: 'claude code桌面版终于可以第三方模型了',
+  meta: {
+    playCount: 2000,
+    publishTime: '2026-4-10 20:21:00',
+    author: '梁其洋',
+    authRequire: true,
+  },
+  video: {
+    poster: 'https://picsum.photos/1280/720?random=1',
+    src: 'https://vjs.zencdn.net/v/oceans.mp4',
+  },
+});
 </script>
 <style lang="scss" scoped>
 @use './index.scss' as *;
