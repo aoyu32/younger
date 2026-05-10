52e12c1de858d1bb316c4bce0f6ba768c7d6d803
“yuyongtao” <3256515381@qq.com>
2026-05-10 14:24:33 +0800

    feat: 全局组件导入配置

---
+++ src/components/index.ts
@@ -0,0 +1,28 @@
+// src/components/index.ts
+import type { App, Component } from 'vue';
+import AoIcon from './ao-icon/index.vue';
+import AoPage from './ao-page/index.vue';
+import AoImage from './ao-page/index.vue';
+import AoButton from './ao-button/index.vue';
+
+const components = [AoIcon, AoPage, AoImage, AoButton];
+export default {
+  install(app: App) {
+    components.forEach((component) => {
+      app.component(component.name!, component);
+    });
+  },
+};
+
+// 自动维护全局导入组件
+// const modules = import.meta.glob('./*/index.vue', { eager: true })
+// export default {
+//   install(app: App) {
+//     Object.entries(modules).forEach(([, module]) => {
+//       const component = (module as any).default as Component
+//       if (component.name) {
+//         app.component(component.name, component)
+//       }
+//     })
+//   }
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@/style/index.scss'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import '@/style/index.scss';
+import AoComponents from '@/components/index';
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(AoComponents);
+app.mount('#app');
