2bdac29dbaef2fea36766845e2d51ffc76041f25
“yuyongtao” <3256515381@qq.com>
2026-06-13 23:44:16 +0800

    feat: 登录注册路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,23 +12,51 @@ const routes = [
         name: 'home',
         path: '/home',
         component: () => import('@/view/home/index.vue'),
+        meta: {
+          title: '首页',
+        },
       },
       {
         name: 'detail',
         path: '/app/detail/:id',
         component: () => import('@/view/app-detail/index.vue'),
+        meta: {
+          title: '应用详情',
+        },
       },
       {
         name: 'video',
         path: '/video/:id',
         component: () => import('@/view/video/index.vue'),
+        meta: {
+          title: '视频详情',
+        },
       },
       {
         name: 'news',
         path: '/news/:id',
         component: () => import('@/view/news/index.vue'),
+        meta: {
+          title: '新闻详情',
+        },
       },
     ],
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/view/auth/login/index.vue'),
+    meta: {
+      title: '登录',
+    },
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: () => import('@/view/auth/register/index.vue'),
+    meta: {
+      title: '注册',
+    },
   },
 ];
 
