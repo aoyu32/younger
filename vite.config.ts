3a8e6544d63a0999e57b3f4f27c274d1d19cc81b
“yuyongtao” <3256515381@qq.com>
2026-06-13 17:33:03 +0800

    fix: 添加环境变量配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) =>
-            tag.startsWith('media-') || tag === 'video-player',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) =>
+              tag.startsWith('media-') || tag === 'video-player',
+          },
         },
-      },
-    }),
-    svgLoader(),
-  ],
-  server: {
-    port: 3001,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }),
+      svgLoader(),
+    ],
+    server: {
+      port: Number(env.VITE_APP_PORT),
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
 });
